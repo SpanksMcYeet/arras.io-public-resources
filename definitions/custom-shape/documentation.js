ac9cee66fb14730d5140853c6263bdd2acd84150
Damocles <lunahackley@gmail.com>
2023-04-15 00:39:24 -0500

    Create documentation.js

---
+++ definitions/custom-shape/documentation.js
@@ -0,0 +1,8 @@
+import Shape from './compiler.js'
+/***
+Documentation and examples of how to use, modify, and build onto the compiler.
+
+The code in the compiler.js file can be placed directly inside of your code, but ensure that it is defined before you access the class
+***/
+
+/*** METHOD EXPLANATION ***/
